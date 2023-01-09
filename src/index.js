a448c65641bd495f054cbb607bce4c1ee63916ed
Martin <nmabregu@gmail.com>
2023-01-09 10:32:04 -0300

    we add route versioning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+// Routes for v1
+const v1 = require('./v1/routes');
 
 app.get('/', (req, res) => {
     res.send('It works!');
 });
+
+app.use('/api/v1', v1);
 
 app.listen(PORT, () => {
     console.log(`Server listening in http://localhost:${PORT}`);
